8c8bca13d693e6c2720a220806e62234d0023fe2
juancarre <juancarre91@gmail.com>
2021-10-20 17:56:53 +0200

    Mailer service

---
+++ mailer/src/Service/Mailer/ClientRoute.php
@@ -0,0 +1,11 @@
+<?php
+
+
+namespace Mailer\Service\Mailer;
+
+
+class ClientRoute
+{
+    public const ACTIVATE_ACCOUNT = 'activate_account';
+
+}
---
+++ mailer/src/Templating/TwigTemplate.php
@@ -0,0 +1,11 @@
+<?php
+
+
+namespace Mailer\Templating;
+
+
+abstract class TwigTemplate
+{
+    public const USER_REGISTER = 'user/register.twig';
+
+}
