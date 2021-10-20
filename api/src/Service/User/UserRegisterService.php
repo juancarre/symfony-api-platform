e81561652516fa82e847fe827a18815112755f87
juancarre <juancarre91@gmail.com>
2021-10-20 10:19:36 +0200

    Envío de mensaje a Rabbit al crear usuario

--- api/src/Service/User/UserRegisterService.php
+++ api/src/Service/User/UserRegisterService.php
@@ -6,10 +6,14 @@ namespace App\Service\User;
 
 use App\Entity\User;
 use App\Exception\User\UserAlreadyExistException;
+use App\Messenger\Message\UserRegisteredMessage;
+use App\Messenger\RoutingKey;
 use App\Repository\UserRepository;
 use App\Service\Password\EncoderService;
 use App\Service\Request\RequestService;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Messenger\Bridge\Amqp\Transport\AmqpStamp;
+use Symfony\Component\Messenger\MessageBusInterface;
 
 class UserRegisterService
 {
@@ -21,16 +25,22 @@ class UserRegisterService
      * @var EncoderService
      */
     private EncoderService $encoderService;
+    /**
+     * @var MessageBusInterface
+     */
+    private MessageBusInterface $messageBus;
 
     /**
      * UserRegisterService constructor.
      * @param UserRepository $userRepository
      * @param EncoderService $encoderService
+     * @param MessageBusInterface $messageBus
      */
-    public function __construct(UserRepository $userRepository, EncoderService $encoderService)
+    public function __construct(UserRepository $userRepository, EncoderService $encoderService, MessageBusInterface $messageBus)
     {
         $this->userRepository = $userRepository;
         $this->encoderService = $encoderService;
+        $this->messageBus = $messageBus;
     }
 
     public function create(Request $request): User
@@ -47,6 +57,11 @@ class UserRegisterService
         } catch (\Exception $exception) {
             throw UserAlreadyExistException::fromEmail($email);
         }
+
+        $this->messageBus->dispatch(
+            new UserRegisteredMessage($user->getId(), $user->getName(), $user->getEmail(), $user->getToken()),
+            [new AmqpStamp(RoutingKey::USER_QUEUE)]
+        );
 
         return $user;
 
