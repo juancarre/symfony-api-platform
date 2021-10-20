d07c690663be8523472feff72d566e7afb34fa84
juancarre <juancarre91@gmail.com>
2021-10-20 11:14:40 +0200

    Configurado servicio mailer para recibir los correos de Rabbit

---
+++ mailer/src/Messenger/Message/UserRegisteredMessage.php
@@ -0,0 +1,57 @@
+<?php
+
+
+namespace Mailer\Messenger\Message;
+
+
+class UserRegisteredMessage
+{
+
+    /**
+     * UserRegisteredMessage constructor.
+     * @param string $id
+     * @param string $name
+     * @param string $email
+     * @param string $token
+     */
+    public function __construct(string $id, string $name, string $email, string $token)
+    {
+        $this->id = $id;
+        $this->name = $name;
+        $this->email = $email;
+        $this->token = $token;
+    }
+
+    /**
+     * @return string
+     */
+    public function getId(): string
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @return string
+     */
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+
+    /**
+     * @return string
+     */
+    public function getToken(): string
+    {
+        return $this->token;
+    }
+
+}
---
+++ mailer/src/Serializer/Messenger/EventSerializer.php
@@ -0,0 +1,36 @@
+<?php
+
+
+namespace Mailer\Serializer\Messenger;
+
+
+use Mailer\Messenger\Message\UserRegisteredMessage;
+use Symfony\Component\Messenger\Envelope;
+use Symfony\Component\Messenger\Transport\Serialization\Serializer;
+
+class EventSerializer extends Serializer
+{
+
+    public function decode(array $encodedEnvelope): Envelope
+    {
+        $translatedType = $this->translateType($encodedEnvelope['headers']['type']);
+
+        $encodedEnvelope['headers']['type'] = $translatedType;
+
+        return parent::decode($encodedEnvelope);
+
+
+    }
+
+    private function translateType(string $type): string
+    {
+        $map = ['App\Messenger\Message\UserRegisteredMessage' => UserRegisteredMessage::class];
+
+        if (array_key_exists($map, $type)) {
+            return $map[$type];
+        }
+
+        return $type;
+    }
+
+}
