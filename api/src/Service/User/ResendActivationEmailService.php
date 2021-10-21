6d704d682a3a77927cf95e248327fba9d542aff5
juancarre <juancarre91@gmail.com>
2021-10-21 19:10:03 +0200

    Servicio reenvio email de activacion terminado

--- api/src/Service/User/ResendActivationEmailService.php
+++ api/src/Service/User/ResendActivationEmailService.php
@@ -4,21 +4,56 @@
 namespace App\Service\User;
 
 
+use App\Exception\User\UserIsActiveException;
+use App\Messenger\Message\UserRegisteredMessage;
+use App\Messenger\RoutingKey;
+use App\Repository\UserRepository;
+use App\Service\Request\RequestService;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Messenger\Bridge\Amqp\Transport\AmqpStamp;
+use Symfony\Component\Messenger\MessageBusInterface;
 
 class ResendActivationEmailService
 {
+    /**
+     * @var UserRepository
+     */
+    private UserRepository $userRepository;
+    /**
+     * @var MessageBusInterface
+     */
+    private MessageBusInterface $messageBus;
 
     /**
      * ResendActivationEmailService constructor.
+     * @param UserRepository $userRepository
+     * @param MessageBusInterface $messageBus
      */
-    public function __construct()
+    public function __construct(UserRepository $userRepository, MessageBusInterface $messageBus)
     {
+        $this->userRepository = $userRepository;
+        $this->messageBus = $messageBus;
     }
 
 
+    /**
+     * @param Request $request
+     */
     public function resend(Request $request): void
     {
+        $email = RequestService::getField($request, 'email');
+        $user = $this->userRepository->findOneByEmailOrFail($email);
 
+        if ($user->isActive()){
+            throw UserIsActiveException::fromEmail($email);
+        }
+
+        $user->setToken(sha1(uniqid()));
+        $this->userRepository->save($user);
+
+        $this->messageBus->dispatch(
+            new UserRegisteredMessage($user->getId(), $user->getName(), $user->getEmail(), $user->getToken()),
+            [new AmqpStamp(RoutingKey::USER_QUEUE)]
+        );
     }
 }
