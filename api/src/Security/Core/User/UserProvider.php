f0763fea78b2db4c42de227f1bd8538c1ea9937a
juancarre <juancarre91@gmail.com>
2021-10-22 11:34:09 +0200

    User provider y configuracion firewall en security.yaml

---
+++ api/src/Security/Core/User/UserProvider.php
@@ -0,0 +1,74 @@
+<?php
+
+
+namespace App\Security\Core\User;
+
+
+use App\Entity\User;
+use App\Exception\User\UserNotFoundException;
+use App\Repository\UserRepository;
+use Doctrine\ORM\OptimisticLockException;
+use Doctrine\ORM\ORMException;
+use Symfony\Component\Security\Core\Exception\UnsupportedUserException;
+use Symfony\Component\Security\Core\Exception\UsernameNotFoundException;
+use Symfony\Component\Security\Core\User\PasswordUpgraderInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Security\Core\User\UserProviderInterface;
+
+class UserProvider implements UserProviderInterface, PasswordUpgraderInterface
+{
+    /**
+     * @var UserRepository
+     */
+    private UserRepository $userRepository;
+
+    /**
+     * UserProvider constructor.
+     * @param UserRepository $userRepository
+     */
+    public function __construct(UserRepository $userRepository)
+    {
+        $this->userRepository = $userRepository;
+    }
+
+    public function loadUserByUsername(string $username): UserInterface
+    {
+        try {
+            return $this->userRepository->findOneByEmailOrFail($username);
+        } catch (UserNotFoundException $e) {
+            throw new UsernameNotFoundException(sprintf('User %s not found', $username));
+        }
+    }
+
+    public function refreshUser(UserInterface $user): UserInterface
+    {
+        if (!$user instanceof User) {
+            throw new UnsupportedUserException(sprintf('instances of %s are not supported', get_class($user)));
+        }
+
+        return $this->loadUserByUsername($user->getUsername());
+    }
+
+    /**
+     * @param UserInterface $user
+     * @param string $newEncodedPassword
+     * @throws ORMException
+     * @throws OptimisticLockException
+     */
+    public function upgradePassword(UserInterface $user, string $newEncodedPassword): void
+    {
+        $user->setPassword($newEncodedPassword);
+
+        $this->userRepository->save($user);
+
+    }
+
+    /**
+     * @param string $class
+     * @return bool
+     */
+    public function supportsClass(string $class): bool
+    {
+        return User::class === $class;
+    }
+}
