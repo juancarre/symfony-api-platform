e03e8d250dacceb62c261b2e8cca9dcd1bde0f64
juancarre <juancarre91@gmail.com>
2021-11-02 19:15:13 +0100

    Configuracion servicios para subir el avatar y guardarlo en el space

--- api/src/Api/Action/User/UploadAvatar.php
+++ api/src/Api/Action/User/UploadAvatar.php
@@ -6,6 +6,8 @@ namespace App\Api\Action\User;
 
 use App\Entity\User;
 use App\Service\User\UploadAvatarService;
+use Doctrine\ORM\OptimisticLockException;
+use Doctrine\ORM\ORMException;
 use Symfony\Component\HttpFoundation\Request;
 
 class UploadAvatar
@@ -24,6 +26,13 @@ class UploadAvatar
         $this->uploadAvatarService = $uploadAvatarService;
     }
 
+    /**
+     * @param Request $request
+     * @param User $user
+     * @return User
+     * @throws ORMException
+     * @throws OptimisticLockException
+     */
     public function __invoke(Request $request, User $user): User
     {
         return $this->uploadAvatarService->uploadAvatar($request, $user);
--- api/src/Service/File/FileService.php
+++ api/src/Service/File/FileService.php
@@ -3,26 +3,63 @@
 
 namespace App\Service\File;
 
-
+use League\Flysystem\FilesystemOperator;
+use Psr\Log\LoggerInterface;
 use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 
 class FileService
 {
+    public const AVATAR_INPUT_NAME = 'avatar';
+
+    private FilesystemOperator $defaultStorage;
+    private LoggerInterface $logger;
+    private string $mediaPath;
 
     /**
      * FileService constructor.
+     * @param FilesystemOperator $defaultStorage
+     * @param LoggerInterface $logger
+     * @param string $mediaPath
      */
-    public function __construct()
+    public function __construct(FilesystemOperator $defaultStorage, LoggerInterface $logger, string $mediaPath)
     {
+        $this->defaultStorage = $defaultStorage;
+        $this->logger = $logger;
+        $this->mediaPath = $mediaPath;
     }
 
     public function uploadFile(UploadedFile $file, string $prefix): string
     {
-        
+        $filename = sprintf('%s/%s.%s', $prefix, sha1(uniqid()), $file->guessExtension());
+
+        $this->defaultStorage->writeStream(
+            $filename,
+            fopen($file->getPathname(), 'r'),
+            ['visibility' => 'public']
+        );
+
+        return $filename;
+    }
+
+    public function validateFile(Request $request, string $inputName): UploadedFile
+    {
+        if (null === $file = $request->files->get($inputName)) {
+            throw new BadRequestHttpException(sprintf('Can not get file with input name: %s', $inputName));
+        }
+
+        return $file;
     }
 
     public function deleteFile(?string $path): void
     {
-        
+        try {
+            if (null === $path) {
+                $this->defaultStorage->delete(explode($this->mediaPath, $path)[1]);
+            }
+        } catch (\Exception $e) {
+            $this->logger->warning(sprintf('File %s not found in the storage', $path));
+        }
     }
 }
