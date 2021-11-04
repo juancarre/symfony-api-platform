a62a4d7a16b558ecd5766a96d0f8159387137e86
juancarre <juancarre91@gmail.com>
2021-11-04 09:30:21 +0100

    Actualizacion de configuracion y aplicacion de Adapter Pattern para cliente facebook

---
+++ api/src/Http/FacebookClient.php
@@ -0,0 +1,43 @@
+<?php
+
+
+namespace App\Http;
+
+
+use Facebook\Exceptions\FacebookSDKException;
+use Facebook\Facebook;
+use Facebook\FacebookResponse;
+
+class FacebookClient
+{
+ private Facebook $client;
+
+    /**
+     * FacebookClient constructor.
+     * @param string $facebookClientId
+     * @param string $facebookSecret
+     * @param string $facebookGraphVersion
+     * @throws FacebookSDKException
+     */
+    public function __construct(string $facebookClientId, string $facebookSecret, string $facebookGraphVersion)
+    {
+        $this->client = new Facebook([
+            'app_id' => $facebookClientId,
+            'app_secret' => $facebookSecret,
+            'default_graph_version' => $facebookGraphVersion
+        ]);
+    }
+
+    /**
+     * @param string $endpoint
+     * @param string $token
+     * @return FacebookResponse
+     * @throws FacebookSDKException
+     */
+    public function get(string $endpoint, string $token): FacebookResponse
+    {
+        $this->client->get($endpoint, $token);
+    }
+
+
+}
