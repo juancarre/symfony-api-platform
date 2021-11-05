27683cdef6938ebb3296f484d1b6f83dcbd79715
juancarre <juancarre91@gmail.com>
2021-11-05 11:18:14 +0100

    - Actualizacion de api platform - Tests funcionales para endpoints de User

---
+++ api/tests/Functional/User/DeleteUserTest.php
@@ -0,0 +1,34 @@
+<?php
+
+
+namespace App\Tests\Functional\User;
+
+
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+class DeleteUserTest extends UserTestBase
+{
+    public function testDeleteUser(): void
+    {
+        self::$peter->request(
+            'DELETE',
+            sprintf('%s/%s', $this->endpoint, $this->getPeterId())
+        );
+
+        $response = self::$peter->getResponse();
+
+        $this->assertEquals(JsonResponse::HTTP_NO_CONTENT, $response->getStatusCode());
+    }
+
+    public function testDeleteAnotherUser(): void
+    {
+        self::$brian->request(
+            'DELETE',
+            sprintf('%s/%s', $this->endpoint, $this->getPeterId())
+        );
+
+        $response = self::$brian->getResponse();
+
+        $this->assertEquals(JsonResponse::HTTP_FORBIDDEN, $response->getStatusCode());
+    }
+}
