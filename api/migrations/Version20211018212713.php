d9ae82f40737703775545f23dcfff935b4b41065
juancarre <juancarre91@gmail.com>
2021-11-10 12:32:43 +0100

    - Se crea migracion para actualizar la base de datos - Se actualiza la configuracio de API Platform

--- api/migrations/Version20211018212713.php
+++ api/migrations/Version20211018212713.php
@@ -7,9 +7,6 @@ namespace DoctrineMigrations;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
-/**
- * Auto-generated Migration: Please modify to your needs!
- */
 final class Version20211018212713 extends AbstractMigration
 {
     public function getDescription(): string
