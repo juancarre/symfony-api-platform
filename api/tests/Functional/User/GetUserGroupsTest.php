38a7fca7a0b1d792ac44369f3b943fc7874901e0
juancarre <juancarre91@gmail.com>
2021-11-10 16:00:58 +0100

    - Se crea Group voter - Extensiones de doctrine para crear subrecursos - Actualizacion de fixtures y testeando endpoints por obtener grupos de usuarios

---
+++ api/tests/Functional/User/GetUserGroupsTest.php
@@ -0,0 +1,32 @@
+<?php
+
+
+namespace App\Tests\Functional\User;
+
+
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+class GetUserGroupsTest extends UserTestBase
+{
+    public function testGetUserGroups(): void
+    {
+        self::$peter->request('GET', sprintf('%s/%s/groups', $this->endpoint, $this->getPeterId()));
+
+        $response = self::$peter->getResponse();
+        $responseData = $this->getResponseData($response);
+
+        $this->assertEquals(JsonResponse::HTTP_OK, $response->getStatusCode());
+        $this->assertCount(1, $responseData['hydra:member']);
+    }
+
+    public function testGetAnotherUserGroups()
+    {
+        self::$brian->request('GET', sprintf('%s/%s/groups', $this->endpoint, $this->getPeterId()));
+
+        $response = self::$brian->getResponse();
+        $responseData = $this->getResponseData($response);
+
+        $this->assertEquals(JsonResponse::HTTP_FORBIDDEN, $response->getStatusCode());
+        $this->assertEquals('You can\'t retrieves another user groups', $responseData['message']);
+    }
+}
